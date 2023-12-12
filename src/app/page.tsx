c0082b8f3721066c77cad1b551f47cbe9b434051
praesidi <edik.halilov1999@gmail.com>
2023-12-12 14:08:24 +0300

    add tailwind and remove boilerplate code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
-import styles from './page.module.css';
-
-console.log(typeof window === 'undefined');
-
 export default function Home() {
 	return (
-		<main className={styles.main}>
-			<div className={styles.description}>test</div>
+		<main className=''>
+			<div className=''>TEST</div>
 		</main>
 	);
 }
