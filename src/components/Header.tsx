2a3e071da72968a97e1474e75773997333998c60
praesidi <edik.halilov1999@gmail.com>
2023-12-13 18:13:52 +0300

    add header

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import ThemeToggle from './ThemeToggle';
+
+export default function Header() {
+	return (
+		<header className='flex flex-col justify-center items-center my-3 relative'>
+			<div className='flex justify-end w-full px-5 -mb-6'>
+				<ThemeToggle className={''} />
+			</div>
+			<h1 className='text-center text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-orange-500 to-pink-600'>
+				Hacker News
+			</h1>
+		</header>
+	);
+}
