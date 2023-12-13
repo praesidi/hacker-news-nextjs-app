ab7bc27a6b58cd5f6d1b281987a7244c2b506ba7
praesidi <edik.halilov1999@gmail.com>
2023-12-13 18:13:43 +0300

    add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,18 @@
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+export default function Footer() {
+	return (
+		<footer className='mt-auto p-1 w-full pt-[40px] text-center'>
+			design and code by{' '}
+			<a
+				href='https://github.com/praesidi'
+				target='_blank'
+				className='text-orange-600 transition-all duration-300 ease-out hover:text-orange-400'
+			>
+				praesidi
+				<FontAwesomeIcon icon={faGithub} className='ml-0.5'></FontAwesomeIcon>
+			</a>
+		</footer>
+	);
+}
