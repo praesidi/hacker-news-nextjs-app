a09d9e89bce63c3ec62f0750bb0437255b6d6b4a
praesidi <edik.halilov1999@gmail.com>
2023-12-15 17:01:07 +0300

    add layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import { config } from '@fortawesome/fontawesome-svg-core';
+config.autoAddCss = false; /* eslint-disable import/first */
+import Footer from '@/components/Footer';
+import Header from '@/components/Header';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -16,7 +21,13 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang='en'>
-			<body className={inter.className}>{children}</body>
+			<body
+				className={`flex flex-col bg-neutral-100 h-screen w-screen ${inter.className}`}
+			>
+				<Header />
+				{children}
+				<Footer />
+			</body>
 		</html>
 	);
 }
