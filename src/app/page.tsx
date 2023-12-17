37bfe6e7e76c4422cb54b946d3d5a7fe27f8e492
praesidi <edik.halilov1999@gmail.com>
2023-12-17 14:54:47 +0300

    update popular news page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
-export default function Home() {
+import NavBar from '@/components/NavBar';
+import PageTitle from '@/components/PageTitle';
+import StoriesContainer from '@/components/StoriesContainer';
+import StoryCard from '@/components/StoryCard';
+import getData from './api/getData';
+
+export default async function Home() {
+	const data = await getData(
+		'https://hacker-news.firebaseio.com/v0/topstories/.json?print=pretty&orderBy=%22$key%22&limitToFirst=100',
+	);
+
 	return (
-		<main className=''>
-			<div className=''>TEST</div>
+		<main className='max-w-[1200px] flex flex-col items-center justify-center'>
+			<NavBar />
+			<PageTitle>News</PageTitle>
+			<StoriesContainer data={data} />
 		</main>
 	);
 }
