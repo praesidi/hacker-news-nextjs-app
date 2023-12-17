68c143fd81b5f1c0e70b99c3b7e5a589fd0ee1d9
praesidi <edik.halilov1999@gmail.com>
2023-12-17 14:53:49 +0300

    add popular news, latest news, job and ask pages

---
+++ src/app/ask/page.tsx
@@ -0,0 +1,18 @@
+import NavBar from '@/components/NavBar';
+import PageTitle from '@/components/PageTitle';
+import StoriesContainer from '@/components/StoriesContainer';
+import getData from '../api/getData';
+
+export default async function Ask() {
+	const data = await getData(
+		'https://hacker-news.firebaseio.com/v0/askstories/.json?print=pretty&orderBy=%22$key%22&limitToFirst=100',
+	);
+
+	return (
+		<main className='max-w-[1200px] flex flex-col items-center justify-center'>
+			<NavBar />
+			<PageTitle>Ask</PageTitle>
+			<StoriesContainer data={data} />
+		</main>
+	);
+}
---
+++ src/app/latest/page.tsx
@@ -0,0 +1,18 @@
+import NavBar from '@/components/NavBar';
+import PageTitle from '@/components/PageTitle';
+import StoriesContainer from '@/components/StoriesContainer';
+import getData from '../api/getData';
+
+export default async function Latest() {
+	const data = await getData(
+		'https://hacker-news.firebaseio.com/v0/newstories/.json?print=pretty&orderBy=%22$key%22&limitToFirst=100',
+	);
+
+	return (
+		<main className='max-w-[1200px] flex flex-col items-center justify-center'>
+			<NavBar />
+			<PageTitle>Latest News</PageTitle>
+			<StoriesContainer data={data} />
+		</main>
+	);
+}
