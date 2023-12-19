ed0624aa51b99d12b3293ac6b9684d86455505c6
praesidi <edik.halilov1999@gmail.com>
2023-12-19 19:51:24 +0300

    change styling and hide comment icon for job posts

--- src/components/StoryCard.tsx
+++ src/components/StoryCard.tsx
@@ -8,6 +8,7 @@ import {
 	faStar,
 } from '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Link from 'next/link';
 
 export default function StoryCard({ data }: { data: Story }) {
 	const postTime = convertTime(data.time);
@@ -16,8 +17,13 @@ export default function StoryCard({ data }: { data: Story }) {
 	return (
 		<div className='w-4/5 mb-2'>
 			<div>
-				<span className='text-lg font-medium text-gray-600'>{data.title}</span>
-				<span className='text-neutral-400 ml-1'>
+				<span className='text-lg font-medium text-gray-600  transition duration-300 hover:text-orange-600 dark:text-gray-400 dark:hover:text-orange-500'>
+					<Link href={`/story/${data.id}`}>{data.title}</Link>
+				</span>
+				<span
+					className={`text-neutral-400 ml-1 transition duration-300 hover:text-neutral-500 dark:text-neutral-600 dark:hover:text-neutral-500 
+					${link === 'link not provided' ? 'pointer-events-none opacity-50' : ''}`}
+				>
 					<a href={data.url} target='_blank'>
 						({link})
 					</a>
@@ -28,19 +34,25 @@ export default function StoryCard({ data }: { data: Story }) {
 					<FontAwesomeIcon icon={faStar} className='text-sm mr-0.5' />
 					{`${data.score} points`}
 				</div>
+
 				<div className='flex items-center shrink-0'>
 					<FontAwesomeIcon icon={faUser} className='text-sm mr-0.5' />
-					{`by ${data.by}`}
+					{data.by}
 				</div>
+
 				<div className='flex items-center shrink-0'>
 					<FontAwesomeIcon icon={faClock} className='text-sm mr-0.5' />
-					{postTime}
+					{`${postTime} ago`}
 				</div>
-				<div className='flex items-center shrink-0'>
-					<FontAwesomeIcon icon={faComments} className='text-sm mr-0.5' />
-					{data.descendants}
-					{data.descendants === 1 ? ' comment' : ' comments'}
-				</div>
+
+				{data.hasOwnProperty('descendants') && (
+					<div className='flex items-center shrink-0'>
+						<FontAwesomeIcon icon={faComments} className='text-sm mr-0.5' />
+						{data.descendants === 1
+							? `${data.descendants} comment`
+							: `${data.descendants} comments`}
+					</div>
+				)}
 			</div>
 		</div>
 	);
