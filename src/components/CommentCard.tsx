2ecaeeb7c758bcb7297e7fbd476a4e25e7fe9372
praesidi <edik.halilov1999@gmail.com>
2023-12-20 20:51:40 +0300

    add nested comments, add ability to show and hide them

--- src/components/CommentCard.tsx
+++ src/components/CommentCard.tsx
@@ -1,26 +1,53 @@
-import { faUser, faClock, faStar } from '@fortawesome/free-regular-svg-icons';
+'use client';
+
+import { faUser, faClock } from '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import sanitizeText from '@/utils/sanitizeText';
 import { Comment } from '@/types';
 import convertTime from '@/utils/convertTime';
+import { ReactNode, useState } from 'react';
 
 export default function CommentCard({ data }: { data: Comment }) {
+	const [areNestedShown, setAreNestedShown] = useState<boolean>(true);
+
 	const sanitizedText = sanitizeText(data.text);
 	const time = convertTime(data.time);
 
-	// TODO: make gaps btw comments even
-
 	return (
 		<>
-			<div className='border-l-2 rounded py-1 px-3 border-neutral-500'>
-				{data.deleted ? (
-					<DeletedComment time={time} />
-				) : (
-					<CommonComment user={data.by} time={time} text={sanitizedText} />
-				)}
+			{/* comment container */}
+			<div className='border rounded-md py-1 px-3 border-transparent border-l-neutral-500  transition duration-500 cursor-pointer hover:border-neutral-500'>
+				{/* comment body */}
+				<div
+					onClick={() => {
+						setAreNestedShown(false);
+					}}
+				>
+					{data.deleted ? (
+						<DeletedComment time={time} />
+					) : (
+						<CommonComment user={data.by} time={time} text={sanitizedText} />
+					)}
+				</div>
+				{/* show nested comments button */}
+				{data.kids && data.kids.length > 0 ? (
+					<ShowNestedCommentsButton
+						areShown={areNestedShown}
+						onClick={() => setAreNestedShown(true)}
+					>
+						{data.kids?.length === 1
+							? '1 comment'
+							: `${data.kids?.length} comments`}
+					</ShowNestedCommentsButton>
+				) : null}
 			</div>
+			{/* nested comments container */}
 			{data.kids && data.kids.length > 0 ? (
-				<div className='flex flex-col gap-3 pl-5 py-1 border-l border-neutral-300 dark:border-gray-700'>
+				<div
+					className={`flex flex-col gap-3 pl-5 py-1 border-l border-neutral-300 dark:border-gray-700 ${
+						areNestedShown ? '' : 'hidden'
+					}`}
+				>
 					{data.kids.map((comment, index) => (
 						<CommentCard key={index} data={comment} />
 					))}
@@ -74,5 +101,26 @@ function DeletedComment({ time }: { time: string }) {
 			</div>
 			<div className='italic text-gray-400'>This comment has been deleted</div>
 		</>
+	);
+}
+
+function ShowNestedCommentsButton({
+	children,
+	areShown,
+	onClick,
+}: {
+	children: ReactNode;
+	areShown: boolean;
+	onClick: () => void;
+}) {
+	return (
+		<div className={`flex justify-start mt-2 ${areShown ? 'hidden' : ''}`}>
+			<button
+				className='w-fit text-orange-400 hover:text-orange-300'
+				onClick={onClick}
+			>
+				{children}
+			</button>
+		</div>
 	);
 }
