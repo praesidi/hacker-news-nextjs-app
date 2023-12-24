c342ff34340f0b246f1e75aff0dd050223793483
praesidi <edik.halilov1999@gmail.com>
2023-12-24 16:01:10 +0300

    fix flash of unstyled content caused by negative margin

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,13 +2,14 @@ import ThemeToggle from './ThemeToggle';
 
 export default function Header() {
 	return (
-		<header className='flex flex-col justify-center items-center w-full my-3 relative'>
-			<div className='flex justify-end w-full px-5 -mb-6'>
-				<ThemeToggle className={''} />
-			</div>
+		<header className='flex justify-between items-center max-w-[1280px] w-full my-3 px-5 relative'>
+			<div className='w-[24px]'></div>
 			<h1 className='text-center text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-orange-500 to-pink-600'>
 				Hacker News
 			</h1>
+			<div className='h-full w-[24px] flex items-start'>
+				<ThemeToggle className={''} />
+			</div>
 		</header>
 	);
 }
